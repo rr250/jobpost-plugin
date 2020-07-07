ab3748fa6173b53dbb01208263f4cf0d39ca0f63
Rishabh Ranjan <rrrishabh7@gmail.com>
2020-07-08 03:45:58 +0530

    button working

--- server/http.go
+++ server/http.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
@@ -40,10 +41,33 @@ func (p *Plugin) handleDialog(w http.ResponseWriter, req *http.Request) {
 	// resume := request.Submission["resume"]
 	// reason := request.Submission["reason"]
 
+	// strs := []string{company.(string), positon.(string), description.(string), skills.(string), experience.(string), location.(string)}
+	// postText := strings.Join(strs, "\n")
+	// log.Printf(postText)
 	postModel := &model.Post{
 		UserId:    request.UserId,
 		ChannelId: request.ChannelId,
 		Message:   company.(string),
+		Props: model.StringInterface{
+			"attachments": []*model.SlackAttachment{
+				{
+					Actions: []*model.PostAction{
+						{
+							Integration: &model.PostActionIntegration{
+								// Context: model.StringInterface{
+								// 	"reminder_id":   r.Reminder.Id,
+								// 	"occurrence_id": r.Reminder.Occurrences[0].Id,
+								// 	"action":        "delete/ephemeral",
+								// },
+								URL: fmt.Sprintf("/plugins/%s/fillform  ", manifest.ID),
+							},
+							Type: model.POST_ACTION_TYPE_BUTTON,
+							Name: "Fill the form",
+						},
+					},
+				},
+			},
+		},
 	}
 
 	_, err5 := p.API.CreatePost(postModel)
